e07f4e3d9d0ecac041cd6d35d39705a12e5a0b04
new <new@192.168.1.8>
2019-10-27 20:01:36 +0530

    1st coding style

---
+++ src/com/app/constructor/Test.java
@@ -0,0 +1,24 @@
+package com.app.constructor;
+
+public class Test { 
+	
+	private int id;
+	private String name;
+	private Double sal;
+	private String place;
+	
+	public Test(int id,String name,Double sal,String place){
+		this.id=id;
+		this.name=name;
+		this.sal=sal;
+		this.place=place;
+		
+	}
+@Override
+	public String toString() {
+		return "Test [id=" + id + ", name=" + name + ", sal=" + sal + ", place=" + place + "]";
+	}
+public static void main(String[] args) {
+	Test t= new Test(10,"madhu",100.0,"hyd");
+}
+}
